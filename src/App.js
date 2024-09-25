409dccf45a0fd383f3903a09089a1857b32101d1
Tamir Alam <tamiralam2@gmai.com>
2024-09-25 16:26:06 +0530

    food-project add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./component/Navbar/Navbar";
+import Home1 from "./component/Home/Home1";
+import Home2 from "./component/Home/Home2";
+import Home3 from "./component/Home/Home3";
+import Footer from "./component/Footer/Footer";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar />
+    <Home1 />
+    <Home2/>
+    <Home3/>
+    <Footer/>
     </div>
   );
 }
---
+++ src/component/Home/Home1.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+  function Home1() {
+    return (
+      <div>
+        <section class="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1770&amp;q=80)] bg-cover bg-center bg-no-repeat">
+          <div class="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
+          <div class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+            <div class="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+              <h1 class="text-3xl font-extrabold sm:text-5xl">
+                Let us find your
+                <strong class="block font-extrabold text-[#49f39e]">
+                  Forever Food.
+                </strong>
+              </h1>
+              <p class="mt-4 max-w-lg sm:text-xl/relaxed">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Nesciunt illo tenetur fuga ducimus numquam ea!
+              </p>
+              <div class="mt-8 flex flex-wrap gap-4 text-center">
+                <a
+                  href="/hotel"
+                  class="block w-full rounded-md bg-[#339966] px-12 py-3 text-sm font-bold text-white shadow hover:bg-[#1c6d45]  focus:outline-none focus:ring active:bg-[#339966] sm:w-auto"
+                >
+                  Search Now
+                </a>
+                <a
+                  href="#"
+                  class="black w-full rounded bg-white px-12 py-3 text-sm font-bold text-[#339966] shadow hover:text-[#47df93] focus:outline-none focus:ring active:text-[#339966] sm:w-auto"
+                >
+                  Know more
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+
+
+export default Home1;
