9d5fdaa20c4fa46ee616181b882034581ee3e248
Tamir Alam <tamiralam2@gmai.com>
2024-09-26 10:07:29 +0530

    img url added

--- src/component/Home/Home1.jsx
+++ src/component/Home/Home1.jsx
@@ -3,7 +3,7 @@ import React from "react";
   function Home1() {
     return (
       <div>
-        <section class="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1770&amp;q=80)] bg-cover bg-center bg-no-repeat">
+        <section class="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat">
           <div class="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
           <div class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
             <div class="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
